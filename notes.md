aeb4288e619bf9519e493f70f3dae6fe33e4b831
Alexandre Quettier <alexandre.quettier@cgi.fr>
2022-04-25 11:29:12 +0200

    Adding cron data controller

--- notes.md
+++ notes.md
@@ -3,7 +3,6 @@ Doc API météo:
 https://openweathermap.org/forecast
 
 TODO:
-- Swagger it
 - Clean docker image built
 - Refactor github actions to use composite
 - Fix action where param is not check then test not run
---
+++ src/cron/controllers/cron.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Logger } from '@nestjs/common';
+import { SchedulerRegistry } from '@nestjs/schedule';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { CRON_NEVER_RUN, MOWER_CRON_NAME } from 'src/assets/cron.constants';
+import { CRON_ROUTE, CRON_STATE_ROUTE } from 'src/assets/route.constants';
+import { CRON_API_TAG } from 'src/assets/swagger.constants';
+import { CronData } from '../models/dto/cron-data.dto';
+
+@ApiTags(CRON_API_TAG)
+@Controller(CRON_ROUTE)
+export class CronController {
+  private readonly _logger = new Logger(CronController.name);
+
+  constructor(private readonly _schedulerRegistry: SchedulerRegistry) {}
+
+  @Get(CRON_STATE_ROUTE)
+  @ApiOkResponse({
+    description: 'Indicates cron jobs data.',
+    type: CronData,
+  })
+  ready(): CronData {
+    this._logger.verbose('Checking cron data.');
+
+    let result: CronData = {};
+    const mowerCronJob = this._schedulerRegistry.getCronJob(MOWER_CRON_NAME);
+    result[MOWER_CRON_NAME] = {
+      lastRun: mowerCronJob.lastDate() ?? CRON_NEVER_RUN,
+      nextRun: mowerCronJob.nextDate(),
+      isRunning: mowerCronJob.running,
+    };
+
+    return result;
+  }
+}
